5635d6d467bc4e08aafdc71547b6427937e912ec
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-27 15:40:41 +0100

    [UPD] initial project structure and README with game description

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//utilities
+/* 
+**********************************************
+ID E CLASSI
+id countdown = countdown
+id frase che ti dice di memorizzare i numeri = istructions
+id lista dei numeri = numbers-list
+id form = answers-form
+div che contiene il gruppo dei numeri = input-group
+tutti gli input hanno class = .form-control 
+bottone conferma classe= .btn
+messaggio finale id = message
+
+**********************************************
+QUALI CLASSI HANNO I TAG CHE MI INTERESSANO
+
+bisogna DARE dopo tot secondi il d-none sia a:
+-countdown id = countdown
+la frase id = istructions
+e la lista dei numeri casuali id = numbers-list
+********
+
+bisogna TOGLIERE il d-none invece dopo tot secondi
+alla form id = answers-form
+
+*/ 
+
+//devo far visualizzare in pagina 5 numeri causali con il for e li faccio visualizzare per 30 secondi
+
+
+
+
+//dopo i 30 secondi metto il d-none agli elementi indicati su e tolgo il d-none agli elementi indicati
+
+
+
+//alla fine il software nel p indicato con id message da la risposta di quanti numeri sono stati indovinati
